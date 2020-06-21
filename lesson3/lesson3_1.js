bb601c5b656dcce7faed5181421c185c8971ef32
KostaKA2020 <konstakozh@yandex.ru>
2020-06-21 18:07:48 +0400

    create lesson3_3

---
+++ lesson3/lesson3_1.js
@@ -0,0 +1,10 @@
+let i = 2; //0 и единица не являются простыми числами
+while (i <= 100) {
+    let n = 2;
+    while (n <= i) {
+        if (i % n == 0) break;
+        if (i - 1 == n) console.log (i);
+        n++;
+    }
+    i++;
+}
