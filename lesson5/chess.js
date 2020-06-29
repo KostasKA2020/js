65b2d4853ba51cd34892ab005cb11439dd835ef6
KostasKA2020 <konstakozh@yandex.ru>
2020-06-29 17:26:55 +0400

    edit lesson5

--- lesson5/chess.js
+++ lesson5/chess.js
@@ -1,18 +1,25 @@
 function boardChess(){
     let chessBlock = document.createElement('div');
         chessBlock.className = 'chess-block';
-        main_border.append(chessBlock);
+        document.body.append(chessBlock);
+        // main_border.append(chessBlock);
     let block;
     let flag = true;
     let figure = {
-        0 : ['&#9820;', '&#9822;', '&#9821;', '&#9819;', '&#9818;','&#9821;', '&#9822;','&#9820;'],
-        1 : ['&#9823;', '&#9823;', '&#9823;', '&#9823;', '&#9823;','&#9823;', '&#9823;','&#9823;'],
-        6 : ['&#9817;', '&#9817;', '&#9817;', '&#9817;', '&#9817;','&#9817;', '&#9817;','&#9817;'],
-        7 : ['&#9814;', '&#9816;', '&#9815;', '&#9813;', '&#9812;','&#9815;', '&#9816;','&#9814;'],
+        0 : ['', 'A', 'B', 'C', 'D','E', 'F','G', 'H', ''],
+        1 : ['8','&#9820;', '&#9822;', '&#9821;', '&#9819;', '&#9818;','&#9821;', '&#9822;','&#9820;','8'],
+        2 : ['7','&#9823;', '&#9823;', '&#9823;', '&#9823;', '&#9823;','&#9823;', '&#9823;','&#9823;','7'],
+        3 : ['6','', '', '', '', '','', '','','6'],
+        4 : ['5','', '', '', '', '','', '','','5'],
+        5 : ['4','', '', '', '', '','', '','','4'],
+        6 : ['3','', '', '', '', '','', '','','3'],
+        7 : ['2','&#9817;', '&#9817;', '&#9817;', '&#9817;', '&#9817;','&#9817;', '&#9817;','&#9817;','2'],
+        8 : ['1','&#9814;', '&#9816;', '&#9815;', '&#9813;', '&#9812;','&#9815;', '&#9816;','&#9814;','1'],
+        9 : ['', 'A', 'B', 'C', 'D','E', 'F','G', 'H', ''],
     };
 
-    for (let i = 0; i<8; i++){
-        for (let j = 0; j<8; j++){
+    for (let i = 0; i<10; i++){
+        for (let j = 0; j<10; j++){
             if (j==0) flag = !flag;
 
             block = document.createElement('div');
@@ -22,13 +29,18 @@ function boardChess(){
 
             if (figure[i]!==undefined && figure[i][j]!==undefined){
                 block.innerHTML = figure[i][j];
-                if (flag) block.className = 'block black figure';
-                else block.className = 'block white figure';
-                // block.className = 'block figure';
-                // block.style.fontFamily = 'Arial';
-                // block.style.fontSize = '60px';
-                // block.style.textAlign = 'center';
-                // block.style.lineHeight = '75px';
+                if ((i==0 && j==0) || (i==0 && j==9) || (i==9 && j==0) || (i==9 && j==9)) {
+                    block.className = 'block border-text numbers letters';
+                } else if (i==0 || i==9) {
+                    if (i==0) block.className = 'block border-text letters rotate';
+                    else block.className = 'block border-text letters';
+                } else if (j==0 || j==9) {
+                    if (j==9) block.className = 'block border-text numbers rotate';
+                    else block.className = 'block border-text numbers';
+                } else {
+                    if (flag) block.className = 'block black figure';
+                    else block.className = 'block white figure';
+                }
             }
 
             chessBlock.appendChild(block);
@@ -37,7 +49,7 @@ function boardChess(){
     }
 }
 
-let main_border = document.createElement('div');
-main_border.className = 'border';
-document.body.append(main_border);
+// let main_border = document.createElement('div');
+// main_border.className = 'border';
+// document.body.append(main_border);
 boardChess();
