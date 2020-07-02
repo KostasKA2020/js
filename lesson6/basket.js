73deb9744f085adfe801d553b95a22369a95e386
KostaKA2020 <konstakozh@yandex.ru>
2020-07-02 22:34:36 +0400

    edit lesson_6

--- lesson6/basket.js
+++ lesson6/basket.js
@@ -21,25 +21,28 @@ document.onclick = event => {
         addSum(event.target.dataset.id);
     }
 }
-
+//Добавляем товары
 let addFunction = id => {
     cart[id];
     let section = document.querySelector(".cart");
     let element = document.createElement("div");
+    let summa = document.getElementById('sum-id');
     element.innerHTML = cart[id].name + " - " + cart[id].price + "руб.";
-    section.append(element);
+    section.insertBefore(element, summa);//вставляем товар перед Суммой
 }
+
+//Добавляем, изменяем и вычисляем Сумму
 let addSum = id => {
     sum += cart[id].price;
     let section = document.querySelector(".cart");
     let content = document.getElementById('sum-id');
     console.log (content);
-    if (content === null) {
+    if (content == null) {
         let summa = document.createElement("div");
-        summa.id = "sum-id;"
-        summa.innerHTML = "Сумма покупок: " + sum;
+        summa.id = "sum-id"
+        summa.innerHTML = "Сумма покупок: " + sum + "руб.";
         section.append(summa);
-    } else summa.innerHTML = "Сумма покупок: " + sum;
+    } else content.innerHTML = "Сумма покупок: " + sum + "руб.";
 }
 
 
