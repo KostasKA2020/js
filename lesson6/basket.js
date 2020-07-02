ef57ff5261c2b030eae94e4397dd9a2da1b81a64
KostaKA2020 <konstakozh@yandex.ru>
2020-07-02 20:53:35 +0400

    edit lesson_6

--- lesson6/basket.js
+++ lesson6/basket.js
@@ -13,6 +13,7 @@ let cart = {
     },
 };
 
+var sum = 0;
 document.onclick = event => {
     console.log(event.target.classList);
     if (event.target.classList.contains("add")) {
@@ -27,24 +28,17 @@ let addFunction = id => {
     let element = document.createElement("div");
     element.innerHTML = cart[id].name + " - " + cart[id].price + "руб.";
     section.append(element);
-    // let sum = cart[id].price;
-    // sum += sum;
-    // if (document.getElementById('sum-id') == null) {
-    //     summa = document.createElement("div");
-    //     summa.innerHTML = "Сумма покупок: " + sum;
-    //     summa.id = "sum-id"
-    //     section.append(summa);
-    // } else summa.innerHTML = "Сумма покупок: " + sum;
 }
 let addSum = id => {
-    cart[id];
+    sum += cart[id].price;
     let section = document.querySelector(".cart");
-    let sum = cart[id].price;
-    sum += sum;
-    summa = document.createElement("div");
-    summa.innerHTML = "Сумма покупок: " + sum;
-    summa.id = "sum-id"
-    section.append(summa);
+    let content = document.getElementById("sum-id");
+    if (content == null) {
+        let summa = document.createElement("div");
+        summa.id = "sum-id;"
+        summa.innerHTML = "Сумма покупок: " + sum;
+        section.append(summa);
+    } else summa.innerHTML = "Сумма покупок: " + sum;
 }
 
 
